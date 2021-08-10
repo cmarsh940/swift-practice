0d2f1f3c1dbdfc77ec3c4875ce77d456f39f3289
Cam <cmarshmollow@gmail.com>
2021-08-10 12:35:52 -0500

    added form practice

---
+++ SwiftUI_Form_Practice/SwiftUI_Form_Practice/SwiftUI_Form_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Form_PracticeApp.swift
+//  SwiftUI_Form_Practice
+//
+//  Created by Cam on 8/10/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Form_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
